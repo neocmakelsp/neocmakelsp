f1d6a92ce5ea457a7fb12bd89c79672de2c6801c
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-07-06 13:06:27 +0800

    update README

--- README.md
+++ README.md
@@ -1,6 +1,36 @@
 # CMake lsp based on Tower and treesitter
 
+[![Crates.io](https://img.shields.io/crates/v/neocmakelsp.svg)](https://crates.io/crates/neocmakelsp)
+
 It is a CMake lsp based on tower-lsp and treesitter 
+
+## Install
+
+```
+cargo install neocmakelsp
+```
+
+## Setup
+
+```lua
+local configs = require("lspconfig.configs")
+local nvim_lsp = require("lspconfig")
+if not configs.neocmake then
+    configs.neocmake = {
+        default_config = {
+            --cmd = { "./target/debug/qmllsp" },
+            cmd = { "./target/debug/neocmakelsp" },
+            filetypes = { "cmake" },
+            root_dir = function(fname)
+                return nvim_lsp.util.find_git_ancestor(fname)
+            end,
+            on_attach = on_attach
+        }
+    }
+    nvim_lsp.neocmake.setup({})
+end
+```
+
 
 ## Features
 
--- settings.lua
+++ settings.lua
@@ -1,18 +1,19 @@
 local configs = require("lspconfig.configs")
 local nvim_lsp = require("lspconfig")
-
-configs.neocmake = {
-    default_config = {
-        --cmd = { "./target/debug/qmllsp" },
-        cmd = { "./target/debug/neocmakelsp" },
-        filetypes = {"cmake"},
-        root_dir = function(fname)
-            return nvim_lsp.util.find_git_ancestor(fname)
-        end,
-        on_attach = function (client, bufnr)
-            vim.notify("Lsp Start")
-            require("cmps.cmp_onattach")(client,bufnr)
-        end
+if not configs.neocmake then
+    configs.neocmake = {
+        default_config = {
+            --cmd = { "./target/debug/qmllsp" },
+            cmd = { "./target/debug/neocmakelsp" },
+            filetypes = { "cmake" },
+            root_dir = function(fname)
+                return nvim_lsp.util.find_git_ancestor(fname)
+            end,
+            on_attach = function(client, bufnr)
+                vim.notify("Lsp Start")
+                require("cmps.cmp_onattach")(client, bufnr)
+            end
+        }
     }
-}
-nvim_lsp.neocmake.setup({})
+    nvim_lsp.neocmake.setup({})
+end
