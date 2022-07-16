c3d7707c0c97d216fb1652457ff552c40755ba89
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-07-16 18:35:11 +0800

    fix complete

--- src/gammer.rs
+++ src/gammer.rs
@@ -134,6 +134,7 @@ fn getsubcoplete(input: tree_sitter::Node, source: &str) -> Option<Vec<Completio
                 let name = &newsource[h][x..y];
                 if name == "set" || name == "SET" {
                     let ids = child.child(2).unwrap();
+                    let h = ids.start_position().row;
                     let x = ids.start_position().column;
                     let y = ids.end_position().column;
                     let name = &newsource[h][x..y];
