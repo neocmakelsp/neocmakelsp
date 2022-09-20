8fbdb866558ceecb2dd57d1f2425c3bb00fcb9e1
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-09-20 23:06:27 +0800

    4.0 prepare

--- src/utils/findpackage.rs
+++ src/utils/findpackage.rs
@@ -11,4 +11,24 @@ mod packagemac;
 #[cfg(target_os = "macos")]
 use packagemac as cmakepackage;
 
+use anyhow::Result;
 pub use cmakepackage::PREFIX;
+use once_cell::sync::Lazy;
+// match file xx.cmake and CMakeLists.txt
+const CMAKEREGEX: Lazy<regex::Regex> = Lazy::new(|| regex::Regex::new(r"^.+\.cmake$|CMakeLists.txt$").unwrap());
+
+// config file
+const CMAKECONFIG: Lazy<regex::Regex> = Lazy::new(|| regex::Regex::new(r"^*Config.cmake$|^*-config.cmake").unwrap());
+#[test]
+fn regextest() {
+    assert!(CMAKEREGEX.is_match("CMakeLists.txt"));
+    assert!(!CMAKEREGEX.is_match("CMakeLists.txtb"));
+    assert!(CMAKEREGEX.is_match("DtkCoreConfig.cmake"));
+    assert!(!CMAKEREGEX.is_match("DtkCoreConfig.cmakeb"));
+}
+#[test]
+fn configtest() {
+    assert!(CMAKECONFIG.is_match("DtkCoreConfig.cmake"));
+    assert!(CMAKECONFIG.is_match("DtkCore-config.cmake"));
+    assert!(!CMAKECONFIG.is_match("DtkCoreconfig.cmake"));
+}
--- src/utils/findpackage/packageunix.rs
+++ src/utils/findpackage/packageunix.rs
@@ -1,4 +1,20 @@
 // here is the logic of findpackage on linux
 //
 pub const PREFIX: [&str; 2] = ["/usr", "/usr/local"];
-
+pub const ARCH: [&str; 1] = ["x86_64-linux-gnu"];
+pub const SHARE: &str = "share";
+pub const LIBS: [&str; 3] = ["lib", "lib32", "lib64"];
+fn temp() {
+    for prefix in PREFIX {
+        for lib in LIBS {
+            if let Ok(paths) = std::fs::read_dir(format!("{}/{}",prefix, lib)) {
+                for path in paths {
+                    if let Ok(pathunit) = path {
+                        if pathunit.metadata().unwrap().is_dir() {
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
