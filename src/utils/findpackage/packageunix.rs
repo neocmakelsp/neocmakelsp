5b6b936d8cab26c4c802678429877f158f1a9efa
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-09-22 18:32:46 +0800

    cargo clippy

--- src/utils/findpackage/packageunix.rs
+++ src/utils/findpackage/packageunix.rs
@@ -16,58 +16,46 @@ pub static CMAKE_PACKAGES: Lazy<Result<Vec<CMakePackage>>> = Lazy::new(|| {
     for prefix in PREFIX {
         for lib in LIBS {
             if let Ok(paths) = std::fs::read_dir(format!("{}/{}/cmake", prefix, lib)) {
-                for path in paths {
-                    if let Ok(pathunit) = path {
-                        let mut version: Option<String> = None;
-                        let mut tojump: Vec<String> = vec![];
-                        let pathname = pathunit.file_name().to_str().unwrap().to_string();
-                        let packagepath = pathunit.path().to_str().unwrap().to_string();
-                        let (packagetype, packagename) = {
-                            if pathunit.metadata().unwrap().is_dir() {
-                                if let Ok(paths) =
-                                    std::fs::read_dir(pathunit.path().to_str().unwrap())
-                                {
-                                    for path in paths {
-                                        if let Ok(pathunit) = path {
-                                            let filepath =
-                                                pathunit.path().to_str().unwrap().to_string();
-                                            if pathunit.metadata().unwrap().is_file() {
-                                                let filename = pathunit
-                                                    .file_name()
-                                                    .to_str()
-                                                    .unwrap()
-                                                    .to_string();
-                                                if CMAKEREGEX.is_match(&filename) {
-                                                    tojump.push(format!("file://{}", filepath));
-                                                    if CMAKECONFIGVERSION.is_match(&filename) {
-                                                        if let Ok(context) =
-                                                            fs::read_to_string(&filepath)
-                                                        {
-                                                            version = get_version(&context);
-                                                        }
-                                                    }
+                for path in paths.flatten() {
+                    let mut version: Option<String> = None;
+                    let mut tojump: Vec<String> = vec![];
+                    let pathname = path.file_name().to_str().unwrap().to_string();
+                    let packagepath = path.path().to_str().unwrap().to_string();
+                    let (packagetype, packagename) = {
+                        if path.metadata().unwrap().is_dir() {
+                            if let Ok(paths) = std::fs::read_dir(path.path().to_str().unwrap()) {
+                                for path in paths.flatten() {
+                                    let filepath = path.path().to_str().unwrap().to_string();
+                                    if path.metadata().unwrap().is_file() {
+                                        let filename =
+                                            path.file_name().to_str().unwrap().to_string();
+                                        if CMAKEREGEX.is_match(&filename) {
+                                            tojump.push(format!("file://{}", filepath));
+                                            if CMAKECONFIGVERSION.is_match(&filename) {
+                                                if let Ok(context) = fs::read_to_string(&filepath) {
+                                                    version = get_version(&context);
                                                 }
                                             }
                                         }
                                     }
                                 }
-                                (FileType::Dir, pathname)
-                            } else {
-                                let filepath = pathunit.path().to_str().unwrap().to_string();
-                                tojump.push(format!("file://{}", filepath));
-                                let pathname =
-                                    pathname.split('.').collect::<Vec<&str>>()[0].to_string();
-                                (FileType::File, pathname)
                             }
-                        };
-                        packages.push(CMakePackage {
-                            name: packagename,
-                            filetype: packagetype,
-                            filepath: packagepath,
-                            version,
-                            tojump,
-                        });
-                    }
+                            (FileType::Dir, pathname)
+                        } else {
+                            let filepath = path.path().to_str().unwrap().to_string();
+                            tojump.push(format!("file://{}", filepath));
+                            let pathname =
+                                pathname.split('.').collect::<Vec<&str>>()[0].to_string();
+                            (FileType::File, pathname)
+                        }
+                    };
+                    packages.push(CMakePackage {
+                        name: packagename,
+                        filetype: packagetype,
+                        filepath: packagepath,
+                        version,
+                        tojump,
+                    });
                 }
             }
         }
@@ -80,60 +68,48 @@ pub static CMAKE_PACKAGES_WITHKEY: Lazy<Result<HashMap<String, CMakePackage>>> =
     for prefix in PREFIX {
         for lib in LIBS {
             if let Ok(paths) = std::fs::read_dir(format!("{}/{}/cmake", prefix, lib)) {
-                for path in paths {
-                    if let Ok(pathunit) = path {
-                        let mut version: Option<String> = None;
-                        let mut tojump: Vec<String> = vec![];
-                        let pathname = pathunit.file_name().to_str().unwrap().to_string();
-                        let packagepath = pathunit.path().to_str().unwrap().to_string();
-                        let (packagetype, packagename) = {
-                            if pathunit.metadata().unwrap().is_dir() {
-                                if let Ok(paths) =
-                                    std::fs::read_dir(pathunit.path().to_str().unwrap())
-                                {
-                                    for path in paths {
-                                        if let Ok(pathunit) = path {
-                                            let filepath =
-                                                pathunit.path().to_str().unwrap().to_string();
-                                            if pathunit.metadata().unwrap().is_file() {
-                                                let filename = pathunit
-                                                    .file_name()
-                                                    .to_str()
-                                                    .unwrap()
-                                                    .to_string();
-                                                if CMAKEREGEX.is_match(&filename) {
-                                                    tojump.push(format!("file://{}", filepath));
-                                                    if CMAKECONFIGVERSION.is_match(&filename) {
-                                                        if let Ok(context) =
-                                                            fs::read_to_string(&filepath)
-                                                        {
-                                                            version = get_version(&context);
-                                                        }
-                                                    }
+                for path in paths.flatten() {
+                    let mut version: Option<String> = None;
+                    let mut tojump: Vec<String> = vec![];
+                    let pathname = path.file_name().to_str().unwrap().to_string();
+                    let packagepath = path.path().to_str().unwrap().to_string();
+                    let (packagetype, packagename) = {
+                        if path.metadata().unwrap().is_dir() {
+                            if let Ok(paths) = std::fs::read_dir(path.path().to_str().unwrap()) {
+                                for path in paths.flatten() {
+                                    let filepath = path.path().to_str().unwrap().to_string();
+                                    if path.metadata().unwrap().is_file() {
+                                        let filename =
+                                            path.file_name().to_str().unwrap().to_string();
+                                        if CMAKEREGEX.is_match(&filename) {
+                                            tojump.push(format!("file://{}", filepath));
+                                            if CMAKECONFIGVERSION.is_match(&filename) {
+                                                if let Ok(context) = fs::read_to_string(&filepath) {
+                                                    version = get_version(&context);
                                                 }
                                             }
                                         }
                                     }
                                 }
-                                (FileType::Dir, pathname)
-                            } else {
-                                let filepath = pathunit.path().to_str().unwrap().to_string();
-                                tojump.push(format!("file://{}", filepath));
-                                let pathname =
-                                    pathname.split('.').collect::<Vec<&str>>()[0].to_string();
-                                (FileType::File, pathname)
                             }
-                        };
-                        packages
-                            .entry(packagename.clone())
-                            .or_insert_with(|| CMakePackage {
-                                name: packagename,
-                                filetype: packagetype,
-                                filepath: packagepath,
-                                version,
-                                tojump,
-                            });
-                    }
+                            (FileType::Dir, pathname)
+                        } else {
+                            let filepath = path.path().to_str().unwrap().to_string();
+                            tojump.push(format!("file://{}", filepath));
+                            let pathname =
+                                pathname.split('.').collect::<Vec<&str>>()[0].to_string();
+                            (FileType::File, pathname)
+                        }
+                    };
+                    packages
+                        .entry(packagename.clone())
+                        .or_insert_with(|| CMakePackage {
+                            name: packagename,
+                            filetype: packagetype,
+                            filepath: packagepath,
+                            version,
+                            tojump,
+                        });
                 }
             }
         }
