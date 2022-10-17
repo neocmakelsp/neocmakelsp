df8b89599e85f03becb77eb1660b2447ad991ea5
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-10-17 22:16:16 +0800

    fix ci

--- src/utils/findpackage/packagemac.rs
+++ src/utils/findpackage/packagemac.rs
@@ -1,1 +1,69 @@
+use std::{collections::HashMap, fs};
+
+use once_cell::sync::Lazy;
+
+use crate::utils::{CMakePackage, FileType};
+
+use super::{get_version, CMAKECONFIGVERSION, CMAKEREGEX};
+
+// here is the logic of findpackage on linux
+//
 pub const PREFIX: [&str; 2] = ["/usr", "/usr/local"];
+pub const LIBS: [&str; 4] = ["lib", "lib32", "lib64", "share"];
+fn get_cmake_message() -> HashMap<String, CMakePackage> {
+    let mut packages: HashMap<String, CMakePackage> = HashMap::new();
+    for prefix in PREFIX {
+        for lib in LIBS {
+            if let Ok(paths) = std::fs::read_dir(format!("{}/{}/cmake", prefix, lib)) {
+                for path in paths.flatten() {
+                    let mut version: Option<String> = None;
+                    let mut tojump: Vec<String> = vec![];
+                    let pathname = path.file_name().to_str().unwrap().to_string();
+                    let packagepath = path.path().to_str().unwrap().to_string();
+                    let (packagetype, packagename) = {
+                        if path.metadata().unwrap().is_dir() {
+                            if let Ok(paths) = std::fs::read_dir(path.path().to_str().unwrap()) {
+                                for path in paths.flatten() {
+                                    let filepath = path.path().to_str().unwrap().to_string();
+                                    if path.metadata().unwrap().is_file() {
+                                        let filename =
+                                            path.file_name().to_str().unwrap().to_string();
+                                        if CMAKEREGEX.is_match(&filename) {
+                                            tojump.push(format!("file://{}", filepath));
+                                            if CMAKECONFIGVERSION.is_match(&filename) {
+                                                if let Ok(context) = fs::read_to_string(&filepath) {
+                                                    version = get_version(&context);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            (FileType::Dir, pathname)
+                        } else {
+                            let filepath = path.path().to_str().unwrap().to_string();
+                            tojump.push(format!("file://{}", filepath));
+                            let pathname =
+                                pathname.split('.').collect::<Vec<&str>>()[0].to_string();
+                            (FileType::File, pathname)
+                        }
+                    };
+                    packages
+                        .entry(packagename.clone())
+                        .or_insert_with(|| CMakePackage {
+                            name: packagename,
+                            filetype: packagetype,
+                            filepath: packagepath,
+                            version,
+                            tojump,
+                        });
+                }
+            }
+        }
+    }
+    packages
+}
+pub static CMAKE_PACKAGES: Lazy<Vec<CMakePackage>> =
+    Lazy::new(|| get_cmake_message().into_values().collect());
+pub static CMAKE_PACKAGES_WITHKEY: Lazy<HashMap<String, CMakePackage>> =
+    Lazy::new(get_cmake_message);
--- src/utils/findpackage/packagewin.rs
+++ src/utils/findpackage/packagewin.rs
@@ -1,1 +1,8 @@
+use std::collections::HashMap;
+use once_cell::sync::Lazy;
+use crate::utils::CMakePackage;
 pub const PREFIX: [&str; 2] = ["C:\\", "D:\\"];
+pub static CMAKE_PACKAGES: Lazy<Vec<CMakePackage>> =
+    Lazy::new(|| vec![]);
+pub static CMAKE_PACKAGES_WITHKEY: Lazy<HashMap<String, CMakePackage>> =
+    Lazy::new(|| HashMap::new());
