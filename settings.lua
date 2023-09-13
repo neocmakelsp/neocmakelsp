de06903fd4e8f8e9b4de3d25398af2a2fcb173b7
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-09-13 11:46:50 +0800

    chore: add init_options
    Issue: https://github.com/Decodetalkers/neocmakelsp/issues/20

--- settings.lua
+++ settings.lua
@@ -8,9 +8,6 @@ return {
             root_dir = function(fname)
                 return nvim_lsp.util.find_git_ancestor(fname)
             end,
-            init_options = {
-                AutomaticWorkspaceInit = true,
-            },
             on_attach = function(client, bufnr)
                 vim.notify("Lsp Start")
                 require("cmps.cmp_onattach")(client, bufnr)
@@ -21,6 +18,9 @@ return {
                         dynamicRegistration = true,
                     },
                 },
+            },
+            init_options = {
+                format = true
             }
         }
     }
