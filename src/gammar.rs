fbaf05a22ff37d5e3b432927cd62409c7b9d8be5
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-11-23 22:36:10 +0800

    chore: better code

--- src/gammar.rs
+++ src/gammar.rs
@@ -30,69 +30,72 @@ pub fn checkerror(local_path: &Path, source: &str, input: tree_sitter::Node) -> 
                 if let Some(mut tran) = checkerror(local_path, source, node) {
                     output.append(&mut tran.inner);
                 }
-                if node.kind() == "normal_command" {
-                    let h = node.start_position().row;
-                    let ids = node.child(0).unwrap();
-                    //let ids = ids.child(2).unwrap();
-                    let x = ids.start_position().column;
-                    let y = ids.end_position().column;
-                    let name = &newsource[h][x..y];
-                    if name.to_lowercase() == "find_package" && node.child_count() >= 4 {
-                        let mut walk = node.walk();
-                        let errorpackages = crate::filewatcher::get_error_packages();
-                        for child in node.children(&mut walk) {
-                            let h = child.start_position().row;
-                            let x = child.start_position().column;
-                            let y = child.end_position().column;
-                            if h < newsource.len() && y > x && y < newsource[h].len() {
-                                let name = &newsource[h][x..y];
-                                if errorpackages.contains(&name.to_string()) {
-                                    output.push((
-                                        child.start_position(),
-                                        child.end_position(),
-                                        "Cannot find such package".to_string(),
-                                        Some(DiagnosticSeverity::ERROR),
-                                    ));
-                                }
+                if node.kind() != "normal_command" {
+                    // INFO: NO NEED TO CHECK ANYMORE
+                    continue;
+                }
+
+                let h = node.start_position().row;
+                let ids = node.child(0).unwrap();
+                //let ids = ids.child(2).unwrap();
+                let x = ids.start_position().column;
+                let y = ids.end_position().column;
+                let name = &newsource[h][x..y];
+                if name.to_lowercase() == "find_package" && node.child_count() >= 4 {
+                    let mut walk = node.walk();
+                    let errorpackages = crate::filewatcher::get_error_packages();
+                    for child in node.children(&mut walk) {
+                        let h = child.start_position().row;
+                        let x = child.start_position().column;
+                        let y = child.end_position().column;
+                        if h < newsource.len() && y > x && y < newsource[h].len() {
+                            let name = &newsource[h][x..y];
+                            if errorpackages.contains(&name.to_string()) {
+                                output.push((
+                                    child.start_position(),
+                                    child.end_position(),
+                                    "Cannot find such package".to_string(),
+                                    Some(DiagnosticSeverity::ERROR),
+                                ));
                             }
                         }
                     }
-                    if name == "include" && node.child_count() >= 4 {
-                        let ids = node.child(2).unwrap();
-                        if ids.start_position().row == ids.end_position().row {
-                            let h = ids.start_position().row;
-                            let x = ids.start_position().column;
-                            let y = ids.end_position().column;
-                            let mut name = &newsource[h][x..y];
-                            if name.split('"').count() != 1 {
-                                let namesplit: Vec<&str> = name.split('"').collect();
-                                name = namesplit[1];
-                            }
-                            if name.contains('$') {
-                                continue;
-                            }
-                            if name.split('.').count() != 1 {
-                                let subpath = local_path.parent().unwrap().join(name);
-                                match cmake_try_exists(&subpath) {
-                                    Ok(true) => {
-                                        if scanner_include_error(&subpath) {
-                                            output.push((
-                                                node.start_position(),
-                                                node.end_position(),
-                                                "Error in include file".to_string(),
-                                                Some(DiagnosticSeverity::ERROR),
-                                            ));
-                                        }
-                                    }
-                                    _ => {
+                }
+                if name == "include" && node.child_count() >= 4 {
+                    let ids = node.child(2).unwrap();
+                    if ids.start_position().row == ids.end_position().row {
+                        let h = ids.start_position().row;
+                        let x = ids.start_position().column;
+                        let y = ids.end_position().column;
+                        let mut name = &newsource[h][x..y];
+                        if name.split('"').count() != 1 {
+                            let namesplit: Vec<&str> = name.split('"').collect();
+                            name = namesplit[1];
+                        }
+                        if name.contains('$') {
+                            continue;
+                        }
+                        if name.split('.').count() != 1 {
+                            let subpath = local_path.parent().unwrap().join(name);
+                            match cmake_try_exists(&subpath) {
+                                Ok(true) => {
+                                    if scanner_include_error(&subpath) {
                                         output.push((
                                             node.start_position(),
                                             node.end_position(),
-                                            "include file does not exist or is inaccessible"
-                                                .to_string(),
-                                            Some(DiagnosticSeverity::WARNING),
+                                            "Error in include file".to_string(),
+                                            Some(DiagnosticSeverity::ERROR),
                                         ));
                                     }
+                                }
+                                _ => {
+                                    output.push((
+                                        node.start_position(),
+                                        node.end_position(),
+                                        "include file does not exist or is inaccessible"
+                                            .to_string(),
+                                        Some(DiagnosticSeverity::WARNING),
+                                    ));
                                 }
                             }
                         }
