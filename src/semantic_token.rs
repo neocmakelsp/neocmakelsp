a9366d05a8851925f57bc157321b6c42d1ed8340
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-06-05 12:45:07 +0800

    chore: record prestart

--- src/semantic_token.rs
+++ src/semantic_token.rs
@@ -2,6 +2,10 @@ use tower_lsp::{
     lsp_types::{SemanticToken, SemanticTokenType, SemanticTokens, SemanticTokensResult},
     Client,
 };
+
+const BOOL_VAL: &[&str] = &["ON", "OFF", "TRUE", "OFF"];
+const UNIQUE_KEYWORD: &[&str] = &["AND", "NOT"];
+
 pub const LEGEND_TYPE: &[SemanticTokenType] = &[
     SemanticTokenType::FUNCTION,
     SemanticTokenType::METHOD,
@@ -28,11 +32,18 @@ pub async fn semantic_token(_client: &Client, context: &str) -> Option<SemanticT
     let tree = thetree?;
     Some(SemanticTokensResult::Tokens(SemanticTokens {
         result_id: None,
-        data: sub_tokens(tree.root_node(), context, &mut 0),
+        data: sub_tokens(tree.root_node(), context, &mut 0, &mut 0, false),
     }))
 }
 
-fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<SemanticToken> {
+fn sub_tokens(
+    input: tree_sitter::Node,
+    source: &str,
+    preline: &mut u32,
+    prestart: &mut u32,
+    is_if: bool,
+) -> Vec<SemanticToken> {
+    let newsource: Vec<&str> = source.lines().collect();
     let mut res = vec![];
 
     let mut course = input.walk();
@@ -40,6 +51,7 @@ fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<
     for child in input.children(&mut course) {
         match child.kind() {
             "normal_command" => {
+                // NOTE: identifier
                 let Some(id) = child.child(0) else {
                     continue;
                 };
@@ -55,6 +67,8 @@ fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<
                     token_modifiers_bitset: 0,
                 });
                 *preline = h as u32;
+
+                res.append(&mut sub_tokens(child, source, preline, prestart, false));
             }
 
             "endmacro_command" | "endif_command" | "endfunction_command" | "else_command" => {
@@ -64,6 +78,9 @@ fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<
                 let h = id.start_position().row;
                 let x = id.start_position().column;
                 let y = id.end_position().column;
+                if h as u32 != *preline {
+                    *prestart = 0;
+                }
                 res.push(SemanticToken {
                     delta_line: h as u32 - *preline,
                     delta_start: x as u32,
@@ -72,11 +89,77 @@ fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<
                     token_modifiers_bitset: 0,
                 });
                 *preline = h as u32;
+            }
+            "argument_list" => {
+                let mut argument_course = child.walk();
+                let mut prestart = 0;
+                for argument in child.children(&mut argument_course) {
+                    let h = argument.start_position().row;
+                    let x = argument.start_position().column;
+                    let y = argument.end_position().column;
+                    if h as u32 != *preline {
+                        prestart = 0;
+                    }
+                    if argument
+                        .child(0)
+                        .is_some_and(|child| child.kind() == "quoted_argument")
+                    {
+                        res.push(SemanticToken {
+                            delta_line: h as u32 - *preline,
+                            delta_start: x as u32 - prestart,
+                            length: (y - x) as u32,
+                            token_type: get_token_position(SemanticTokenType::STRING),
+                            token_modifiers_bitset: 0,
+                        });
+                        prestart = x as u32;
+                        *preline = h as u32;
+                        continue;
+                    }
+                    let name = &newsource[h][x..y];
+                    if BOOL_VAL.contains(&name) {
+                        res.push(SemanticToken {
+                            delta_line: h as u32 - *preline,
+                            delta_start: x as u32 - prestart,
+                            length: (y - x) as u32,
+                            token_type: get_token_position(SemanticTokenType::VARIABLE),
+                            token_modifiers_bitset: 0,
+                        });
+                        prestart = x as u32;
+                        *preline = h as u32;
+                        continue;
+                    }
+                    if UNIQUE_KEYWORD.contains(&name) {
+                        res.push(SemanticToken {
+                            delta_line: h as u32 - *preline,
+                            delta_start: x as u32 - prestart,
+                            length: (y - x) as u32,
+                            token_type: get_token_position(SemanticTokenType::KEYWORD),
+                            token_modifiers_bitset: 0,
+                        });
+                        prestart = x as u32;
+                        *preline = h as u32;
+                        continue;
+                    }
+                    if name.chars().all(|a| !a.is_lowercase()) && !is_if {
+                        res.push(SemanticToken {
+                            delta_line: h as u32 - *preline,
+                            delta_start: x as u32 - prestart,
+                            length: (y - x) as u32,
+                            token_type: get_token_position(SemanticTokenType::KEYWORD),
+                            token_modifiers_bitset: 0,
+                        });
+                        prestart = x as u32;
+                        *preline = h as u32;
+                    }
+                }
             }
             "function" | "macro" | "if" => {
                 let h = child.start_position().row;
                 let x = child.start_position().column;
                 let y = child.end_position().column;
+                if h as u32 != *preline {
+                    *prestart = 0;
+                }
                 res.push(SemanticToken {
                     delta_line: h as u32 - *preline,
                     delta_start: x as u32,
@@ -85,10 +168,17 @@ fn sub_tokens(input: tree_sitter::Node, source: &str, preline: &mut u32) -> Vec<
                     token_modifiers_bitset: 0,
                 });
                 *preline = h as u32;
-                res.append(&mut sub_tokens(child, source, preline));
+                res.append(&mut sub_tokens(child, source, preline, prestart, false));
             }
-            "body" | "macro_def" | "function_def" | "if_condition" | "if_command" | "function_command" | "macro_command"=> {
-                res.append(&mut sub_tokens(child, source, preline));
+            "body" | "macro_def" | "function_def" | "if_condition" | "if_command"
+            | "function_command" | "macro_command" => {
+                res.append(&mut sub_tokens(
+                    child,
+                    source,
+                    preline,
+                    prestart,
+                    child.kind() == "if_command",
+                ));
             }
             _ => {}
         }
