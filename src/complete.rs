neocmakelsp/neocmakelsp

commit aa06cc6625271d6831d1792c414442b604f2a7b8

Fix crash when completing for `pkg_check_modules`. (#69)
In some cases, `start_position` and `end_position` are in different rows, and the column of `end_position` may be smaller than `start_position`, leading to a crash when accessing `newsource[h][x..y]`.

src/complete.rs
@@ -483,8 +483,23 @@ fn getsubcomplete(
                                 let ids = child.child(2).unwrap();
                                 let x = ids.start_position().column;
                                 let y = ids.end_position().column;
-                                let package_names: Vec<&str> =
-                                    newsource[h][x..y].split(' ').collect();
+
+                                let mut row = ids.start_position().row;
+                                let row_start = ids.start_position().row;
+                                let row_end = ids.end_position().row;
+                                let mut names: String = newsource[row][x..].to_string();
+                                row += 1;
+
+                                while row < row_end {
+                                    names = format!("{} {}", names, newsource[row].to_string());
+                                    row += 1;
+                                }
+
+                                if row != row_start {
+                                    names = format!("{} {}", names, newsource[row][..y].to_string())
+                                }
+
+                                let package_names: Vec<&str> = names.split(' ').collect();
                                 let package_name = package_names[0];
 
                                 let modernpkgconfig = package_names.contains(&PKG_IMPORT_TARGET);

commit 523aad0fdd61ea8888d699c835594b72ac644f2b

fix: issue caused by `aa06cc6`

src/complete.rs
@@ -484,21 +484,26 @@ fn getsubcomplete(
                                 let x = ids.start_position().column;
                                 let y = ids.end_position().column;
 
-                                let mut row = ids.start_position().row;
                                 let row_start = ids.start_position().row;
                                 let row_end = ids.end_position().row;
-                                let mut names: String = newsource[row][x..].to_string();
-                                row += 1;
-
-                                while row < row_end {
-                                    names = format!("{} {}", names, newsource[row]);
+                                let mut names: String;
+                                if row_start == row_end {
+                                    names = newsource[h][x..y].to_string();
+                                } else {
+                                    let mut row = row_start;
+                                    names = newsource[row][x..].to_string();
                                     row += 1;
-                                }
 
-                                if row != row_start {
-                                    names = format!("{} {}", names, &newsource[row][..y])
-                                }
+                                    while row < row_end {
+                                        names = format!("{} {}", names, newsource[row]);
+                                        row += 1;
+                                    }
 
+                                    if row != row_start {
+                                        assert_eq!(row, row_end);
+                                        names = format!("{} {}", names, &newsource[row][..y])
+                                    }
+                                }
                                 let package_names: Vec<&str> = names.split(' ').collect();
                                 let package_name = package_names[0];
 