8d9b61684157a87ee2b4f25fdc0590c92b69aa81
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-08-10 19:23:07 +0859

    fix: lock problem

--- src/utils/findpackage/vcpkg.rs
+++ src/utils/findpackage/vcpkg.rs
@@ -26,10 +26,8 @@ pub static VCPKG_LIBS: LazyLock<Arc<Mutex<Vec<&str>>>> =
 
 fn get_available_libs() -> Vec<PathBuf> {
     let mut ava: Vec<PathBuf> = vec![];
-    let vcpkg_prefix = VCPKG_PREFIX.lock().unwrap();
-    let vcpkg_libs = VCPKG_LIBS.lock().unwrap();
-    for prefix in vcpkg_prefix.iter() {
-        for lib in vcpkg_libs.iter() {
+    for prefix in VCPKG_PREFIX.lock().unwrap().iter() {
+        for lib in VCPKG_LIBS.lock().unwrap().iter() {
             let p = Path::new(prefix).join(lib);
             if p.exists() {
                 ava.push(p);
@@ -41,8 +39,7 @@ fn get_available_libs() -> Vec<PathBuf> {
 
 fn get_cmake_message() -> HashMap<String, CMakePackage> {
     let mut packages: HashMap<String, CMakePackage> = HashMap::new();
-    let vcpkg_prefix = VCPKG_PREFIX.lock().unwrap();
-    for lib in vcpkg_prefix.iter() {
+    for lib in VCPKG_PREFIX.lock().unwrap().iter() {
         let Ok(paths) = glob::glob(&format!("{lib}/share/*/cmake/")) else {
             continue;
         };
