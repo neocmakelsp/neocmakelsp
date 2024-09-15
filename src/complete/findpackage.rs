980bff0b5b08f35623c82a9e5b1f6c86d84031da
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-09-16 00:11:55 +0900

    feat: use fakedata to unit test

--- src/complete/findpackage.rs
+++ src/complete/findpackage.rs
@@ -21,6 +21,7 @@ static FIND_PACKAGE_SPACE_KEYWORDS: LazyLock<Vec<CompletionItem>> = LazyLock::ne
     ]
 });
 
+#[cfg(unix)]
 static PKGCONFIG_KEYWORDS: LazyLock<Vec<CompletionItem>> = LazyLock::new(|| {
     vec![
         CompletionItem {
@@ -62,16 +63,10 @@ pub static CMAKE_SOURCE: LazyLock<Vec<CompletionItem>> = LazyLock::new(|| {
         .collect()
 });
 
-// NOTE: remove who start with space
-// FIXME: I do not know how to do unit test here. Because the value in lazy will just init once.
-// I use moc way unit test the place in hover, so the global value is init there. Then here I
-// cannot set the test data. So I cannot comeout a better way to do the unit test which related
-// with the global values
 pub(super) fn completion_items_with_prefix(space: &str) -> Vec<CompletionItem> {
     let mut data: Vec<CompletionItem> = CACHE_CMAKE_PACKAGES
         .iter()
-        .map(|package| Some((package.name.strip_prefix(space)?, package)))
-        .flatten()
+        .filter_map(|package| Some((package.name.strip_prefix(space)?, package)))
         .map(|(label, package)| CompletionItem {
             label: label.to_string(),
             kind: Some(CompletionItemKind::MODULE),
@@ -91,6 +86,55 @@ pub(super) fn completion_items_with_prefix(space: &str) -> Vec<CompletionItem> {
         .collect();
     data.append(&mut FIND_PACKAGE_SPACE_KEYWORDS.clone());
     data
+}
+
+#[test]
+fn test_prefix() {
+    use crate::utils::{CMakePackage, CMakePackageFrom, PackageType};
+    use crate::Url;
+    use std::path::Path;
+    let data = completion_items_with_prefix("bash");
+
+    let data_package = CMakePackage {
+        name: "bash-completion-fake".to_string(),
+        packagetype: PackageType::Dir,
+        #[cfg(unix)]
+        location: Url::from_file_path("/usr/share/bash-completion").unwrap(),
+        #[cfg(not(unix))]
+        location: Url::from_file_path(r"C:\Develop\bash-completion-fake").unwrap(),
+        version: None,
+        #[cfg(unix)]
+        tojump: vec![
+            Path::new("/usr/share/bash-completion/bash_completion-fake-config.cmake").to_path_buf(),
+        ],
+        #[cfg(not(unix))]
+        tojump: vec![Path::new(
+            r"C:\Develop\bash-completion-fake\bash-completion-fake-config.cmake",
+        )
+        .to_path_buf()],
+        from: CMakePackageFrom::System,
+    };
+    let result_item = CompletionItem {
+        label: "-completion-fake".to_string(),
+        kind: Some(CompletionItemKind::MODULE),
+        detail: Some("Module".to_string()),
+        documentation: Some(Documentation::String(match &data_package.version {
+            None => format!(
+                "name: {}\nFiletype: {}\nFrom: {}\n",
+                data_package.name, data_package.packagetype, data_package.from
+            ),
+            Some(version) => format!(
+                "name: {}\nFiletype: {}\nFrom: {}\nversion: {}",
+                data_package.name, data_package.packagetype, data_package.from, version
+            ),
+        })),
+        ..Default::default()
+    };
+
+    let mut result_data = vec![result_item];
+    result_data.append(&mut FIND_PACKAGE_SPACE_KEYWORDS.clone());
+
+    assert_eq!(data, result_data);
 }
 
 #[cfg(unix)]
