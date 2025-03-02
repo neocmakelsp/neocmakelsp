af8c052223ba6826a3c6000979e008285130b6b5
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-03-02 21:23:35 +0900

    chore: bump tree-sitter-cmake

--- src/consts.rs
+++ src/consts.rs
@@ -1,4 +1,4 @@
 use std::sync::LazyLock;
 
 pub static TREESITTER_CMAKE_LANGUAGE: LazyLock<tree_sitter::Language> =
-    LazyLock::new(tree_sitter_cmake::language);
+    LazyLock::new(|| tree_sitter_cmake::LANGUAGE.into());
