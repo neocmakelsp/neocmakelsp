bf1a1123afaf96333cd47900e949e74281a12347
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-07-25 12:31:59 +0900

    chore: update unit test
    add file name regex test

--- src/fileapi/target.rs
+++ src/fileapi/target.rs
@@ -1,6 +1,24 @@
 use crate::fileapi::ApiVersion;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::LazyLock};
+
+static TARGET_REGEX: LazyLock<regex::Regex> = LazyLock::new(|| {
+    regex::Regex::new(r"(target-(?P<name>[0-9a-zA-Z]+)(-(?P<target>[a-zA-z]+))?-.+.json)").unwrap()
+});
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Target {
+    pub build_type: BuildType,
+    pub info: TargetInfo,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum BuildType {
+    Debug,
+    Release,
+    None,
+    Other(String),
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Artifact {
@@ -40,7 +58,7 @@ pub struct Source {
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Target {
+pub struct TargetInfo {
     artifacts: Vec<Artifact>,
     codemodel_version: ApiVersion,
     compile_groups: Option<Vec<CompileGroup>>,
@@ -51,7 +69,7 @@ pub struct Target {
     _others: HashMap<String, serde_json::Value>,
 }
 
-impl Target {
+impl TargetInfo {
     pub fn target_type(&self) -> TargetType {
         if self.type_ == "EXECUTABLE" {
             return TargetType::Executable;
@@ -71,13 +89,26 @@ pub enum TargetType {
 
 #[cfg(test)]
 mod test {
-    use crate::fileapi::target::{Target, TargetType};
+    use crate::fileapi::target::{TARGET_REGEX, TargetInfo, TargetType};
 
     #[test]
     fn target_serde() {
         let file = include_str!("../../assets_for_test/waycrate.json");
-        let target: Target = serde_json::from_str(file).unwrap();
+        let target: TargetInfo = serde_json::from_str(file).unwrap();
         assert_eq!(target.target_type(), TargetType::Executable);
         assert_eq!(target.artifacts[0].path, "waycratelock");
+    }
+
+    #[test]
+    fn build_type_read() {
+        let file_name = "target-waycratelock-Debug-7d1c13a099b19b474ca1.json";
+        let caps = TARGET_REGEX.captures(file_name).unwrap();
+
+        assert_eq!(&caps["name"], "waycratelock");
+        assert_eq!(&caps["target"], "Debug");
+        let file_name = "target-waycratelock-7d1c13a099b19b474ca1.json";
+        let caps = TARGET_REGEX.captures(file_name).unwrap();
+        assert_eq!(&caps["name"], "waycratelock");
+        assert_eq!(caps.name("target"), None);
     }
 }
