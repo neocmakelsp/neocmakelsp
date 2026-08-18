9a60691d9ca9d0f1acb9899a301db1acc47386d5
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-08-18 22:56:17 +0900

    feat: add version.json to control version

---
+++ src/utils/version.rs
@@ -0,0 +1,168 @@
+use std::sync::LazyLock;
+
+#[derive(Debug, Clone, Copy, Default, Eq, PartialEq)]
+pub struct Version {
+    pub major: u32,
+    pub minor: u32,
+    pub patch: u32,
+    pub extra: Option<Extra>,
+}
+
+const EXTRRA_REGEX_RULES: &str = r"((?<version>[0-9]+)(?<extra>[a-z]+)(?<extraVersion>[0-9]+))";
+const EXTRRA_REGEX: LazyLock<regex::Regex> =
+    LazyLock::new(|| regex::Regex::new(EXTRRA_REGEX_RULES).unwrap());
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub enum Extra {
+    Alpha(u32),
+    Beta(u32),
+    Rc(u32),
+}
+
+impl Version {
+    pub const fn invalid() -> Self {
+        Self {
+            major: 0,
+            minor: 0,
+            patch: 0,
+            extra: None,
+        }
+    }
+}
+
+impl From<&str> for Version {
+    fn from(value: &str) -> Self {
+        let mut version = Version::default();
+        let versions: Vec<&str> = value.split(".").collect();
+        for (index, ver) in versions.iter().enumerate() {
+            let ver: u32 = if index != 2 {
+                let Ok(ver) = ver.parse() else {
+                    return Version::default();
+                };
+                ver
+            } else {
+                match ver.parse() {
+                    Ok(ver) => ver,
+                    Err(_) if let Some(caps) = EXTRRA_REGEX.captures(ver) => {
+                        let ver = &caps["version"];
+                        let extra = &caps["extra"];
+
+                        let extra_version = &caps["extraVersion"];
+                        let extra_version: u32 = extra_version.parse().unwrap();
+                        let extra = match extra {
+                            "alpha" => Extra::Alpha(extra_version),
+                            "beta" => Extra::Beta(extra_version),
+                            "rc" => Extra::Rc(extra_version),
+                            _ => return Version::default(),
+                        };
+                        version.extra = Some(extra);
+
+                        ver.parse().unwrap()
+                    }
+                    _ => return Version::default(),
+                }
+            };
+
+            match index {
+                0 => version.major = ver,
+                1 => version.minor = ver,
+                2 => version.patch = ver,
+                _ => {
+                    break;
+                }
+            }
+        }
+        version
+    }
+}
+
+impl From<String> for Version {
+    fn from(value: String) -> Self {
+        let mut version = Version::default();
+        let versions: Vec<&str> = value.split(".").collect();
+        for (index, ver) in versions.iter().enumerate() {
+            let Ok(ver) = ver.parse() else {
+                return Version::default();
+            };
+            match index {
+                0 => version.major = ver,
+                1 => version.minor = ver,
+                2 => version.patch = ver,
+                _ => {
+                    break;
+                }
+            }
+        }
+        version
+    }
+}
+
+impl Ord for Version {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        use std::cmp::Ordering;
+        if self.major > other.major {
+            return Ordering::Greater;
+        } else if self.major < other.major {
+            return Ordering::Less;
+        }
+        if self.minor > other.minor {
+            return Ordering::Greater;
+        } else if self.minor < other.minor {
+            return Ordering::Less;
+        }
+        if self.patch > other.patch {
+            return Ordering::Greater;
+        } else if self.patch < other.patch {
+            return Ordering::Less;
+        }
+        return Ordering::Equal;
+    }
+}
+
+impl PartialOrd for Version {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Extra;
+    use super::Version;
+
+    const INVALAD_VERSION: Version = Version::invalid();
+    #[test]
+    fn invalid() {
+        let version = "abcd";
+        let version: Version = version.into();
+        assert_eq!(version, INVALAD_VERSION);
+        let version = "1.3.ab";
+        let version: Version = version.into();
+        assert_eq!(version, INVALAD_VERSION);
+    }
+    #[test]
+    fn valid() {
+        let version = "1.2.3";
+        let version: Version = version.into();
+        assert_eq!(
+            version,
+            Version {
+                major: 1,
+                minor: 2,
+                patch: 3,
+                extra: None
+            }
+        );
+        let version = "1.3.3rc1";
+        let version: Version = version.into();
+        assert_eq!(
+            version,
+            Version {
+                major: 1,
+                minor: 3,
+                patch: 3,
+                extra: Some(Extra::Rc(1))
+            }
+        );
+    }
+}
