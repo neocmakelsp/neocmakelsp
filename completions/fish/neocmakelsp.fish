e49b46d5a97e449b35574bdfb1b05bfca21b73ee
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-09-14 11:23:07 +0900

    chore: update completions

--- completions/fish/neocmakelsp.fish
+++ completions/fish/neocmakelsp.fish
@@ -1,1 +1,1 @@
-COMPLETE=fish neocmakelsp | source
+complete --keep-order --exclusive --command neocmakelsp --arguments "(COMPLETE=fish neocmakelsp -- (commandline --current-process --tokenize --cut-at-cursor) (commandline --current-token))"
